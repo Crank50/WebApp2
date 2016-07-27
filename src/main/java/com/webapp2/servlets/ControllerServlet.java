919277e3ad2dab202e12780c333a76feb18c8c63
Justin <Crank50@icloud.com>
2016-07-27 11:23:41 -0600

    WebApp2

--- src/main/java/com/webapp2/servlets/ControllerServlet.java
+++ src/main/java/com/webapp2/servlets/ControllerServlet.java
@@ -1,5 +1,6 @@
 package com.webapp2.servlets;
 
+import com.webapp2.model.User;
 import com.webapp2.model.myData;
 
 import javax.servlet.RequestDispatcher;
@@ -8,11 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by Justin on 7/27/16.
  */
 public class ControllerServlet extends HttpServlet{
+
+    public static HashMap<String, User> users;
+
+    public ControllerServlet() {
+        users = new HashMap<>();
+        User u1 = new User("Sam","Sam likes ham...");
+        users.put(u1.getName(),u1);
+        User u2 = new User("Tom","Tom is tall...");
+        users.put(u2.getName(),u2);
+        User u3 = new User("Terry","Terry is tenacious...");
+        users.put(u3.getName(),u3);
+        User u4 = new User("Sally","Sally listens to The Smiths...");
+        users.put(u4.getName(),u4);
+    }
+
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException {
         String uri = request.getRequestURI();
@@ -21,7 +38,21 @@ public class ControllerServlet extends HttpServlet{
         String jspName = uri.substring(uri.lastIndexOf('/')+1);
         System.out.println("JSP Name: "+jspName);
 
+
         //start Bl
+
+        if(jspName.equalsIgnoreCase("userStuff")) {
+
+            String name = request.getParameter("name");
+            if(name != null && !name.isEmpty() && users.containsKey(name)) {
+                User u = users.get(name);
+                request.setAttribute("name",u.getName());
+                request.setAttribute("message",u.getSpecialMessage());
+                request.setAttribute("firstTime",u.getFirstLogin());
+            } else {
+                jspName = "login";
+            }
+        }
         myData myData = new myData();
         request.setAttribute("what",myData.getWhat());
 
